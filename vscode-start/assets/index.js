68f71253421518c40ea0aa59886d6b238394ad19
AmirShaygordanov <116222144+AmirShaygordanov@users.noreply.github.com>
2022-11-15 19:32:21 +0300

    Add files via upload

---
+++ vscode-start/assets/index.js
@@ -0,0 +1,164 @@
+"use strict"
+/* Первое задание
+let name = prompt("Как тебя зовут?")
+alert(`Здраствуй, ${name}`)
+*/
+
+/* Второе задание (c = 2, b = 1)
+let a = 1, b = 1;
+
+let c = ++a;
+let d = b++;
+
+alert(c);
+alert(d);
+*/
+
+/* Третье задание (x = 5)
+let a = 2;
+let x = 1 + (a *=2);
+alert(x);
+*/
+
+/* Четвёртое задание
+alert ("" + 1 + 0); //10
+alert ("" -1 + 0); //-1
+alert (true + false); //1
+alert (6 / "3"); //2
+alert ("2" * "3"); //6
+alert (4 + 5 + "px"); //9px
+alert ("$" + 4 + 5); //$45
+alert ("4" - 2); //2
+alert ("4px" - 2); //NaN
+alert ("   -9  " + 5); // -9  5
+alert ("   -9  " - 5); // -14
+alert (null + 1); // 1
+alert (undefined + 1); //NaN
+alert (" \t \n" - 2); //-2
+*/
+
+/* Пятое задание
+let a = 1;
+let b = 2;
+alert(a + b); //3
+*/
+
+/* Шестое задание
+let num = 3;
+alert(num);
+*/
+
+/* Седьмое задание
+let a =10, b = 2; 
+alert(a + b); // 12
+alert(a - b); // 8
+alert(a * b); // 20
+alert(a / b); // 5
+*/
+
+/* Восьмое задание
+let c = 15, d = 2;
+let result = c + d;
+alert(result); // 17
+*/
+
+/* Дувятое задание
+let a = 10, b = 2, c = 5;
+alert(a + b + c); // 17
+*/
+
+/*  Десятое задание
+let a = 17, b = 10, d = 7;
+let c = a - b;
+let result = c + d;
+alert (result);
+*/
+
+/* Одиннадцатое задание
+let str = "Привет, Мир!";
+alert (str);
+*/
+
+/* Двенадцатое задание
+let str1 = "Привет,", str2 = " Мир!";
+alert(str1 + str2);
+*/
+
+/* Тринадцатое задание
+let name = "Amir";
+alert (`Привет,  ${name}`);
+*/
+
+/* Четырнадцатое задание
+let age = 16;
+alert (`Мне ${age} лет`);
+*/
+
+/* Пятнадцатое задание
+let name = prompt("Как тебя зовут?")
+alert(`Ваше имя, ${name}`)
+*/
+
+/* Шестнадцатое задание
+let num = prompt(" Напишите любое число, и я возведу его в квадрат");
+alert(num * num);
+*/
+
+/* Семнадцатое задание
+let str = "abcde";
+alert(str[0]);
+alert(str[2]);
+alert(str[4]);
+*/
+
+/* Восемнадцатое задание
+let num = String(12345);
+alert(num[0] * num[1] * num[2] * num[3] * num[4]);
+*/
+
+/* Девятнадцатое задание 
+let quesHour = confirm("Тебе нужно перевести часы в секунды?")
+if (quesHour == true){
+    let hour = prompt("Напиши кол-во часов, которые нужно перевести");
+    alert(`Это будет ${hour * 60 * 60}`);
+} else{
+    let quesDays = confirm("Тебе нужно перевести сутки в секунды?");
+    if (quesDays == true) {
+        let days = prompt("Напиши кол-во суток, которые нужно перевести в секунды");
+        alert(`Это будет ${days * 24 * 60 * 60}`);
+    } else {
+        let month = prompt("Тогда напиши сколько месяцев мне перевести в секунды");
+        alert(`Это будет ${month * 30 * 24 * 60 * 60}`);
+    }
+} 
+*/
+
+/* Двадцатое задание
+let hour = prompt("Который сейчас час?");
+let min = prompt("А минута?");
+let sec = prompt("А секeнда?");
+alert (`Сейчас ${hour}:${min}:${sec}`);
+*/
+
+/*
+let num = 4;
+alert(num * num);
+*/
+
+/* Двадцать первое задание
+var num = 47;
+num += 7;
+num -= 18;
+num *= 10;
+num /= 15;
+alert(num)
+*/
+
+/*
+var num = 10;
+num ++;
+num++;
+num--;
+alert(num);
+*/
+
